1c638520527ba09a70002f99f272cf6e7aa9b98c
vladgrudak <ralkavladislav@gmail.com>
2020-04-11 20:02:46 +0300

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Владислав Ралка on 11.04.2020.
+//  Copyright © 2020 Grudak team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
