fe0c98c552a26ce96327cbcf8d8bf942057428bb
vladgrudak <ralkavladislav@gmail.com>
2020-04-11 21:50:21 +0300

    End of lesson

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
+        
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func goToNextElement(_ sender: UIButton) {
+        if currentElementIndex < elementList.count - 1 {
+            currentElementIndex += 1
+        } else {
+            currentElementIndex = 0
+        }
+        updateElement()
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    struct ChemicalElement {
+        let symbol: String
+        let name: String
+        let atomicWeight: Int
+        let imageName: String
+    }
+    
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        imageView.image = UIImage(named: elementList[currentElementIndex])
+    }
+    
 }
 
